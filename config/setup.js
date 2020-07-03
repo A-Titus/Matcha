0a1e337d254c2447ee4a053fad7474865aa86251
Mlungisi MPOFU <mlmpofu@c4r23s3.wethinkcode.co.za>
2020-07-03 16:04:06 +0200

    working on it

---
+++ config/setup.js
@@ -0,0 +1,58 @@
+function database() {
+  var mysql = require("mysql");
+  var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "mludakriss5128",
+  });
+  con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query("CREATE DATABASE IF NOT EXISTS matcha", function (err, result) {
+      if (err) throw err;
+      console.log("Database created!");
+    });
+  });
+}
+
+ function table() {
+  var mysql = require("mysql");
+  var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "mludakriss5128",
+    database: "matcha",
+  });
+    con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected");
+    var sql =
+      "CREATE TABLE IF NOT EXISTS user(id int AUTO_INCREMENT PRIMARY KEY , name varchar(255),surname varchar(255),username varchar(255),email varchar(255),password varchar(255), verified boolean, setup boolean)";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("user Table created");
+    });
+
+    var sql = "CREATE TABLE IF NOT EXISTS images (img_id int(11) AUTO_INCREMENT PRIMARY KEY, image_name varchar(255) , image_path varchar(255) , username varchar(255), profile_pic boolean)";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("image Table created");
+    });
+    
+    var sql = "CREATE TABLE IF NOT EXISTS user_profile (id int AUTO_INCREMENT PRIMARY KEY, gender varchar(255) , pref_gender varchar(255) , bio varchar(255), age int(11), username varchar(255))";
+      con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("user_profile Table created");
+      });
+
+      var sql = "CREATE TABLE IF NOT EXIST messages (id int AUTO_INCREMENT PRIMARY KEY , sender TEXT NOT NULL , receiver TEXT NOT NULL , message TEXT NOT NULL)";
+      con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("message Table created");
+      });
+
+  });
+
+}
+database();
+setTimeout(table, 3500);
---
+++ src/registration.js
@@ -0,0 +1,142 @@
+var express = require("express");
+var mysql = require("mysql");
+var bodyParser = require("body-parser");
+var nodemailer = require('nodemailer');
+
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+var router = express.Router();
+var bcrypt = require("bcrypt");
+var con = require("../config/connection");
+
+con.connect(function (err) {
+  if (err) {
+    console.log("Unable to connect to the database");
+  } else {
+    console.log("Connection to the database successful");
+  }
+});
+
+router.get("/", function (req, res) {
+  res.render("registration");
+});
+
+router.post("/", urlencodedParser, function (req, res) {
+  var name = req.body.name;
+  var surname = req.body.surname;
+  var username = req.body.username;
+  var email = req.body.email;
+  var password = req.body.password;
+  var conf_password = req.body.conf_password;
+
+  if (!name || !surname || !username || !email || !password || !conf_password) {
+    console.log("all fields must be completed");
+    res.render("registration");
+    return;
+  }
+
+  //DO VALIDATIONS FOR NAME AND SURNAME
+
+  if (
+    !req.body.email.match(
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )
+  ) {
+    console.log("The format of the email address is incorrect");
+    res.render("registration");
+    return;
+  }
+
+  if (!req.body.password.match(/^[A-Za-z]\w{8,20}$/)) {
+    console.log(
+      "Password must be atleast 8 characters long containing an uppercase character, lowecase character and a number "
+    );
+    res.render("registration");
+    return;
+  }
+
+  if (req.body.password !== req.body.conf_password) {
+    console.log("Passswords do not match");
+    res.render("registration");
+    return;
+  } else {
+    var hashed_pass = bcrypt.hashSync(password, 15);
+    console.log(hashed_pass);
+  }
+
+  //check if username already exists.
+  var sql =
+    "SELECT count(*) as total FROM user WHERE username = ? OR email = ?";
+  var query = con.query(sql, [username, email], function (err, result) {
+    console.log("Total Records:- " + result[0].total);
+    if (result[0].total >= 1) {
+      console.log("username or email already in use");
+      res.render("registration");
+      return;
+    } else {
+      var user_record = {
+        name: name,
+        surname: surname,
+        username: username,
+        email: email,
+        password: hashed_pass,
+        verified: '0',
+        setup: '0',
+      };
+
+      var profile_record = {
+        gender: null,
+        pref_gender: null,
+        bio: null,
+        age: null,
+        username: username,
+      };
+
+      con.query("INSERT INTO user_profile SET?", profile_record, function (err, result) {
+        if (err) {
+          status = "Unable to create this user";
+          console.log(status);
+          console.log(err);
+        }
+        })
+
+      con.query("INSERT INTO user SET?", user_record, function (err, result) {
+        if (err) {
+          status = "Unable to create this user";
+          console.log(status);
+          console.log(err);
+          res.render("registration");
+        } else {
+            // var transporter = nodemailer.createTransport({
+            //     service: 'gmail',
+            //     auth: {
+            //       user: 'abdussamadtitus@gmail.com',
+            //       pass: 'Titusat@2000'
+            //     },
+            //     tls: { rejectUnauthorized: false }
+            //   });
+              
+            //   var mailOptions = {
+            //     from: 'youremail@gmail.com',
+            //     to: 'waliwir382@vewku.com',
+            //     subject: 'Sending Email using Node.js',
+            //     text: 'That was easy!'
+            //   };
+              
+            //   transporter.sendMail(mailOptions, function(error, info){
+            //     if (error) {
+            //       console.log(error);
+            //     } else {
+            //       console.log('Email sent: ' + info.response);
+            //     }
+            //   });  
+
+          res.redirect("/login");
+        }
+      });
+    }
+  });
+});
+
+module.exports = router;
